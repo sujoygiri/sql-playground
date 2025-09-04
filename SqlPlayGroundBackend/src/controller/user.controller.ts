e38dfca60a457e8a7f6106b05a292fb7cbca802f
Sujoy Giri <girisujoy630@gmail.com>
2025-09-04 16:35:57 +0530

    added required util functions

--- SqlPlayGroundBackend/src/controller/user.controller.ts
+++ SqlPlayGroundBackend/src/controller/user.controller.ts
@@ -1,9 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import crypto from "node:crypto"
+
 
 export const createUserRole = (req: Request, res: Response, next: NextFunction) => {
-    const userSessionId = crypto.randomBytes(10).toString("hex");
-    const hashedPassword = crypto.scryptSync(userSessionId,"salt",10).toString("hex")
-    const createUserRoleQuery = `CREATE ROLE ${userSessionId} WITH LOGIN PASSWORD ${hashedPassword}`;
+    // const createUserRoleQuery = `CREATE ROLE ${userSessionId} WITH LOGIN PASSWORD ${hashedPassword}`;
     
 }
---
+++ SqlPlayGroundBackend/src/utils/global.util.ts
@@ -0,0 +1,42 @@
+import crypto from "node:crypto"
+
+import { poolQuery } from "../db/connection.db";
+
+function getHashedPassword(sessionId: string): string {
+    const salt = String(process.env.SALT);
+    const hashedPassword = crypto.scryptSync(sessionId,salt,10).toString("hex");
+    return hashedPassword;
+}
+
+async function roleExistOrCreate(sessionId: string) {
+    try {
+        const isRoleExistQuery = `SELECT rolname FROM pg_roles WHERE rolname=$1`;
+        const isRoleExistQueryResult = await poolQuery(isRoleExistQuery,[sessionId]);
+        if(isRoleExistQueryResult.rowCount !== 0) {
+            return;
+        } else {
+            const hashedPassword = getHashedPassword(sessionId);
+            const roleCreationQuery = `CREATE ROLE $1 WITH LOGIN PASSWORD $2 CONNECTION LIMIT 5`;
+            await poolQuery(roleCreationQuery,[sessionId, hashedPassword]);
+        }
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+}
+
+async function dbExistOrCreate(sessionId: string) {
+    try {
+        const isDbExistQuery = `SELECT datname FROM pg_database WHERE datname=$1`;
+        const isDbExistQueryResult = await poolQuery(isDbExistQuery,[sessionId]);
+        if(isDbExistQueryResult.rowCount !== 0) {
+            return;
+        } else {
+            const dbCreationQuery = `CREATE DATABASE $1 WITH OWNER=$1 CONNECTION LIMIT=5`;
+            await poolQuery(dbCreationQuery,[sessionId]);
+        }
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+}
